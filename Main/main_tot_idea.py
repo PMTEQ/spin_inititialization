9ce25e8f37d67e9414cd190fd35fd329b61a0d74
CorentinMorinSorbonne <84860479+CorentinMorinSorbonne@users.noreply.github.com>
2022-11-24 12:08:24 +0100

    Add files via upload

---
+++ Main/main_tot_idea.py
@@ -0,0 +1,67 @@
+from DG645 import dg645
+from PicoHarp_Coding_class import PH
+from my_aerodiode_class import my_tombak
+from ctypes import *  
+import sys
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+#%% Important informations for the code
+
+HOST_DG645 = b"192.168.1.103" #DG645 IP addres
+COM_tombak = 'COM5'
+#%% Set the cycles options
+
+N_samples = 1000
+vect_delay = [1,10,20,30,40,50]
+
+#%% Opening the devices
+
+mydg=dg645() 
+myph=PH()
+mytombak = my_tombak()
+
+myph.open_ph()
+mydg.open_ip(HOST_DG645)
+mytombak.open_my_tombak(COM_tombak)
+
+#%% Set the resolution of the PH
+
+#Range:
+    # 0, Resolution 4ps, Time span ; 262.1 ns
+    # 1, Resolution 8ps, Time span ; 524.3 ns
+    # 2, Resolution 16ps, Time span ; 1.049 us
+    # 3, Resolution 32ps, Time span ; 2.097 us
+    # 4, Resolution 64ps, Time span ; 4.194 us
+    # 5, Resolution 128ps, Time span ; 8.389 us
+    # 6, Resolution 256ps, Time span ; 16.777 us
+    # 7, Resolution 512ps, Time span ; 33.554 us
+#/!\ Default range is 0
+
+#Pour des sondes de 5us pourquoi pas une reso de 128ps? 
+
+myph.set_range(0)
+
+#%% Larger of the pulses
+
+larg_sonde = 5 * 1e-6
+
+larg_gate = larg_sonde + (2*20)*1e-9 #Larger of the gate for the APD
+mydg.set_larg_gate(larg_gate) #Setting the larger of the gate
+
+larg_pump = 50 * 1e-6
+
+#%% Physical cable delays
+
+delay_sync_trig=84*1e-9 
+delay_sync_gate=-67.5*1e-9
+
+
+#%% Boucle de mesure
+
+#%% Close all
+
+mytombak.off_out()
+myph.close_APD()
+mydg.close()
---
+++ Main/my_aerodiode_class.py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from aerodiode import Tombak
+import time
+
+# set_freq_divider=100000
+# set_delay_pulse=0
+# set_pulse_width = 100
+
+class my_tombak():
+    
+    def __init__(self):
+        #Found by myself
+        self.INSTRUCT_FUNCTIONING_MODE_OFF = 0
+        self.PULSE_IN_PHOTODIODE = 3
+        
+    def open_my_tombak(self, COM):
+        
+        self.tombak = Tombak(COM)
+        
+    def close_my_tombak(self):
+        self.tombak.set_status_instruction(self.tombak.INSTRUCT_FUNCTIONING_MODE, self.INSTRUCT_FUNCTIONING_MODE_OFF)
+        self.tombak.apply_all()
+        self.tombak.close()
+
+    def read_my_config(self):
+
+
+        print("Read Configuration")
+        print("INSTRUCT_FUNCTIONING_MODE = ",
+        self.tombak.read_status_instruction(self.tombak.INSTRUCT_FUNCTIONING_MODE))
+        print("INSTRUCT_PULSE_IN_SRC = ", self.tombak.read_status_instruction(self.tombak.INSTRUCT_PULSE_IN_SRC))
+        print("INSTRUCT_PULSE_IN_FREQUENCY_DIV = ",
+        self.tombak.read_freq_instruction(self.tombak.INSTRUCT_PULSE_IN_FREQUENCY_DIV))
+        print("INSTRUCT_SYNC_OUT_1_SRC = ",
+        self.tombak.read_status_instruction(self.tombak.INSTRUCT_SYNC_OUT_1_SRC))
+        print("INSTRUCT_PULSE_OUT_WIDTH = ",
+        self.tombak.read_time_instruction(self.tombak.INSTRUCT_PULSE_OUT_WIDTH))
+        
+    def pulse_gene_no_gate_trig_clock_sync_1_trigger(self, delay_pulse, width_pulse, divider):
+        
+        set_functionning_mode = self.tombak.PULSE_GENERATOR
+        
+        print("Set Configuration")
+        
+        self.tombak.set_status_instruction(self.tombak.INSTRUCT_FUNCTIONING_MODE, set_functionning_mode)
+        self.tombak.set_status_instruction(self.tombak.INSTRUCT_PULSE_IN_SRC, self.tombak.DIRECT)#ok
+        self.tombak.set_status_instruction(self.tombak.INSTRUCT_TRIGGER_SRC, self.tombak.INT)#
+        self.tombak.set_status_instruction(self.tombak.INSTRUCT_SYNC_OUT_1_SRC, self.tombak.TRIGGER)#ok
+        self.tombak.set_status_instruction(self.tombak.INSTRUCT_GATE_CONTROL, self.tombak.NO_GATE)#ok
+        self.tombak.set_time_instruction(self.tombak.INSTRUCT_PULSE_OUT_WIDTH, width_pulse)#ok
+        self.tombak.set_time_instruction(self.tombak.INSTRUCT_PULSE_OUT_DELAY, delay_pulse)#ok
+        self.tombak.set_integer_instruction(self.tombak.INSTRUCT_PULSE_IN_FREQUENCY_DIV, divider)
+
+        self.tombak.apply_all()
+        print("Set Configuration terminated")
+        
+    def pulse_gene_no_gate_trig_PD_sync_1_trigger(self, delay_pulse, width_pulse, divider, threshold):
+        
+        
+        print("Set Configuration")
+        
+        set_functionning_mode = 1
+        
+        self.tombak.set_status_instruction(self.tombak.INSTRUCT_FUNCTIONING_MODE, set_functionning_mode)
+
+        self.tombak.set_status_instruction(self.tombak.INSTRUCT_PULSE_IN_SRC, self.PULSE_IN_PHOTODIODE )#ok
+        self.tombak.set_status_instruction(self.tombak.INSTRUCT_TRIGGER_SRC, self.tombak.INT)#
+        self.tombak.set_status_instruction(self.tombak.INSTRUCT_SYNC_OUT_1_SRC, self.tombak.TRIGGER)#ok
+        self.tombak.set_status_instruction(self.tombak.INSTRUCT_GATE_CONTROL, self.tombak.NO_GATE)#ok
+        self.tombak.set_time_instruction(self.tombak.INSTRUCT_PULSE_OUT_WIDTH, width_pulse)#ok
+        self.tombak.set_time_instruction(self.tombak.INSTRUCT_PULSE_OUT_DELAY, delay_pulse)#ok
+        self.tombak.set_integer_instruction(self.tombak.INSTRUCT_PULSE_IN_FREQUENCY_DIV, divider)
+        self.tombak.set_voltage_instruction(self.tombak.INSTRUCT_PULSE_IN_THRESHOLD,threshold)
+
+        self.tombak.apply_all()
+        print("Set Configuration terminated")
+        
+    def off_out(self):
+        self.tombak.set_status_instruction(self.tombak.INSTRUCT_FUNCTIONING_MODE, self.INSTRUCT_FUNCTIONING_MODE_OFF)
+        self.tombak.apply_all()
+
+
+# print("Read Configuration")
+# print("INSTRUCT_FUNCTIONING_MODE = ",
+# tombak.read_status_instruction(tombak.INSTRUCT_FUNCTIONING_MODE))
+# print("INSTRUCT_PULSE_IN_SRC = ", tombak.read_status_instruction(tombak.INSTRUCT_PULSE_IN_SRC))
+# print("INSTRUCT_PULSE_IN_FREQUENCY_DIV = ",
+# tombak.read_freq_instruction(tombak.INSTRUCT_PULSE_IN_FREQUENCY_DIV))
+# print("INSTRUCT_SYNC_OUT_1_SRC = ",
+# tombak.read_status_instruction(tombak.INSTRUCT_SYNC_OUT_1_SRC))
+# print("INSTRUCT_PULSE_OUT_WIDTH = ",
+# tombak.read_time_instruction(tombak.INSTRUCT_PULSE_OUT_WIDTH))
+
+# time.sleep(15)
+
+# tombak.set_status_instruction(tombak.INSTRUCT_FUNCTIONING_MODE, INSTRUCT_FUNCTIONING_MODE_OFF)
+# tombak.apply_all()
+
+
+# tombak.close()
+
