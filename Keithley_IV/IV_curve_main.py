8da39bbfdfe59d2a101dd692f16c0acb32331440
CorentinMorinSorbonne <84860479+CorentinMorinSorbonne@users.noreply.github.com>
2022-12-14 18:24:38 +0100

    Update IV_curve_main.py

--- Keithley_IV/IV_curve_main.py
+++ Keithley_IV/IV_curve_main.py
@@ -10,20 +10,35 @@ from os.path import exists
 #%% /!\ A remplir: 
     
 info_datas='commercial_diode'#Informations sur les donnees
-V_deb=-0.1 # Tension de d2but de mesure
-V_fin=0.5 #Tension de fin de mesure
-N_ech=10 # Nomnre de mesures a prendre
+V_deb=-1 # Tension de d2but de mesure
+V_fin=0 #Tension de fin de mesure
+N_ech=31 # Nomnre de mesures a prendre
+
+keithley_model=2 #1=2401 et 2=2604
+
+channel_meas='B' #A ou B
+
+if (channel_meas=='A'):
+    source_str='smua'
+elif (channel_meas=='B'):
+    source_str='smub'
+
 #%% Ouverture du Keithley
 rm = visa.ResourceManager()
 print (rm.list_resources())
-with rm. open_resource('COM7') as Keithley:
-    Keithley.port = 'COM7'
-    Keithley.baudrate = 9600
-    Keithley.timeout = 25000
+
+if keithley_model==1:
+    with rm. open_resource('COM3') as Keithley:
+        Keithley.port = 'COM3'
+        Keithley.baudrate = 9600
+        Keithley.timeout = 25000
+        Keithley.open()
+        Keithley.read_termination = '\r'
+        Keithley.write_termination = '\r'
+elif  keithley_model==2:
+    Keithley = rm.open_resource('ASRL3::INSTR')
     
-Keithley.open()
-Keithley.read_termination = '\r'
-Keithley.write_termination = '\r'
+
 Keithley.write("*RST")
 Keithley.write("*IDN?")
 Q=Keithley.read()
@@ -36,19 +51,46 @@ volt=np.linspace(V_deb,V_fin,N_ech) #Definition de toutes les tensions a explore
 res_volt=[] #Vecteur qui contiendra la tension appliquee
 res_amp=[] #Vecteur qui contiendra le courant mesure
 
-Keithley.write(":SOUR:VOLT:LEV:IMM:AMPL 0") #Mise a 0V de la tension pour etre sur
-Keithley.write(":OUTP ON")    #Allumage de loutput
+if keithley_model==1:
+    Keithley.write(":SOUR:VOLT:LEV:IMM:AMPL 0") #Mise a 0V de la tension pour etre sur
+    Keithley.write(":OUTP ON")    #Allumage de loutput
 
-for i in range(len(volt)): #Boucle pour les mesures
-    Keithley.write(":SOUR:VOLT:LEV:IMM:AMPL "+str(volt[i]))  #Application de la tension
-    Keithley.write(":MEAS:CURR:DC?") #Commande de mesure
-    Q=Keithley.read() #La mesure est un tableau, la premiere case est la tension, la seconde le courant
-    A=np.array(Q.split(','))
-    A=A.astype('float64')
-    cur_volt=A[0] #Stockage des donnees
-    cur_amp=A[1]
-    res_volt.append(cur_volt)#Stockage des donnees
-    res_amp.append(cur_amp)
+    for i in range(len(volt)): #Boucle pour les mesures
+        Keithley.write(":SOUR:VOLT:LEV:IMM:AMPL "+str(volt[i]))  #Application de la tension
+        Keithley.write(":MEAS:CURR:DC?") #Commande de mesure
+        Q=Keithley.read() #La mesure est un tableau, la premiere case est la tension, la seconde le courant
+        A=np.array(Q.split(','))
+        A=A.astype('float64')
+        cur_volt=A[0] #Stockage des donnees
+        cur_amp=A[1]
+        res_volt.append(cur_volt)#Stockage des donnees
+        res_amp.append(cur_amp)
+    Keithley.write(":SOUR:VOLT:LEV:IMM:AMPL 0")
+
+elif  keithley_model==2:
+    
+    Keithley.write(source_str+".source.func="+source_str+".OUTPUT_DCVOLTS")
+    Keithley.write(source_str+".source.levelv=0")
+    Keithley.write(source_str+".source.output ="+ source_str+".OUTPUT_ON")
+                   
+
+    for i in range(len(volt)): #Boucle pour les mesures
+        Keithley.write(source_str+".source.levelv="+str(volt[i]))  #Application de la tension
+        Keithley.write('print('+source_str+".measure.i())") #Commande de mesure
+        Q=Keithley.read() #La mesure est un tableau, la premiere case est la tension, la seconde le courant
+        A=float((Q.replace('\n','')))
+        cur_amp=A
+        
+        Keithley.write('print('+source_str+".measure.v())") #Commande de mesure
+        Q=Keithley.read() #La mesure est un tableau, la premiere case est la tension, la seconde le courant
+        A=float((Q.replace('\n','')))
+        cur_volt=A
+        
+        res_volt.append(cur_volt)#Stockage des donnees
+        res_amp.append(cur_amp)
+    
+    Keithley.write(source_str+".source.output ="+ source_str+".OUTPUT_OFF")
+
     
 plt.plot(res_volt,res_amp)
 
@@ -76,6 +118,5 @@ with open(file_name, 'w') as f: #Enregistrement
 #             data.append(row[0].split(' '))
 # data=np.array(data).astype('float64') #Donn2es exploitables
 #%%
-Keithley.write(":SOUR:VOLT:LEV:IMM:AMPL 0")
 
 Keithley.close()
